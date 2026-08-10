848192ef077709686906f6d873d57a457839b612
alinakhan9076 <alinak.dev04@gmail.com>
2026-08-10 20:52:29 +0530

    Update UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,8 +59,7 @@ function HomePage({ tasks, setTasks }) {
     return(
         <div className="min-h-screen bg-slate-100 px-4 py-8">
             <div className="mx-auto max-w-2xl rounded-2xl bg-white p-6 shadow-xl">
-                <h1 className="mb-6 text-3xl font-bold text-slate-800">
-                    TaskNest</h1>
+
             <AddTaskForm onAdd={addTask}/>
 
             <FilterBar filter={filter}
